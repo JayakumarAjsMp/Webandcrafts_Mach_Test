9cc65b90182b251eb71be2c090b2374b3f662520
Jayakumar Arasan <3vaystechnologies@gmail.com>
2023-10-01 10:24:03 +0530

    Initial Commit

---
+++ Shopping/ShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingApp.swift
+//  Shopping
+//
+//  Created by INI SYSTEMS on 01/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
