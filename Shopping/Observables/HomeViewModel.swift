07a7d91116678f16bbb03200d887173c6e8871fc
Jayakumar Arasan <3vaystechnologies@gmail.com>
2023-10-01 14:42:42 +0530

    Initial commit

---
+++ Shopping/Observables/HomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeObserver.swift
+//  Shopping
+//
+//  Created by INI SYSTEMS on 01/10/23.
+//
+
+import Foundation
---
+++ Shopping/UI/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Shopping
+//
+//  Created by INI SYSTEMS on 01/10/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
